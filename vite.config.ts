60b7285ca429b5421f86856bf9d5368b473b66c7
zhangxx <zhangxx@4dstc.com>
2025-05-13 16:18:08 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cicd-vue/', // 设置 base 为仓库名
   plugins: [vue()],
-})
+});
